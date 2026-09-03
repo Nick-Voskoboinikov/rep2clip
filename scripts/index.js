98723808eb7245ac3681dac964b7886a28a35989
Nick Voskoboinikov <Nick@Voskoboinikov.com>
2026-09-03 21:13:22 +0300

    introducing settings

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,39 @@
 
+function recallSettings(){
+    if (localStorage.getItem("prefs") === null) {
+      console.log('No prefs set previously, creating anew...');
+      let arrPrefs={"theme":"Тёмная","decimal separator":",","reported strings":{"Киоск, продажи и Киоск, чек": true, "Напитки": true,"Саб дня 15см": true,"Саб дня 30см": true,"Комбо Трио": true},"measurement units":{"Люди":" 👤","Люди, много":" 👥","Деньги":" ₽","Доставки":" 📦","Сэндвичи 15см":" 🥪","Сэндвичи 30см":" 🥖","Напитки":" 🥤","Комбо-трио":" 🥖🥤🍪"}}
+      let jsonedPrefs=JSON.stringify(arrPrefs);;
+      localStorage.setItem("prefs", jsonedPrefs);
+    }
+    let jsonedPrefsRead=JSON.parse(localStorage.getItem("prefs"));
+    // console.log(jsonedPrefsRead);
+    document.querySelector('#pref_decimal_separator').value=jsonedPrefsRead['decimal separator'];
+    let pref_theme_selector=document.querySelector('#pref_theme');
+    // let pref_theme_selectedOption = [...pref_theme_selector.options].find(
+    //   option => option.value === jsonedPrefsRead['theme']
+    // );
+    // console.log(pref_theme_selectedOption);
+    pref_theme_selector.value = jsonedPrefsRead['theme'];
+    document.querySelector('#pref_peeps').value=jsonedPrefsRead['measurement units']['Люди'];
+    document.querySelector('#pref_peeps_lots').value=jsonedPrefsRead['measurement units']['Люди, много'];
+    document.querySelector('#pref_cash').value=jsonedPrefsRead['measurement units']['Деньги'];
+    document.querySelector('#pref_deliveries').value=jsonedPrefsRead['measurement units']['Доставки'];
+    document.querySelector('#pref_sixinches').value=jsonedPrefsRead['measurement units']['Сэндвичи 15см'];
+    document.querySelector('#pref_footlongs').value=jsonedPrefsRead['measurement units']['Сэндвичи 30см'];
+    document.querySelector('#pref_trios').value=jsonedPrefsRead['measurement units']['Комбо-трио'];
+    document.querySelector('#pref_drinks').value=jsonedPrefsRead['measurement units']['Напитки'];
+    // console.log(jsonedPrefsRead['measurement units']);
+
+
+    console.log(jsonedPrefsRead['reported strings']);
+    // console.log((JSON.parse(localStorage.getItem("prefs")))['measurement units']['Деньги']);
+}
+
+function fixDecimals(numToFix){
+ return (String(numToFix)).replace(".", (JSON.parse(localStorage.getItem("prefs")))['decimal separator']);
+}
+
 function goClip(){
 const d = new Date();
 let datey;
@@ -38,22 +73,25 @@ if(isNaN(kioskSum)){kioskSum=0;}
 let kioskGuests = parseInt((document.querySelector('#kioskGuests').value), 10);
 if(isNaN(kioskGuests)){kioskGuests=0;}
 let sixInchesOfTheDay = parseInt((document.querySelector('#sixInchesOfTheDay').value), 10);
+if(isNaN(sixInchesOfTheDay)){sixInchesOfTheDay=0;}
 let footlongsOfTheDay = parseInt((document.querySelector('#footlongsOfTheDay').value), 10);
+if(isNaN(footlongsOfTheDay)){footlongsOfTheDay=0;}
 let triosN = parseInt((document.querySelector('#trio').value), 10);
 if(isNaN(triosN)){triosN=0;}
 
 let kioskCheque = (kioskGuests === 0) ? 0 : (kioskSum/kioskGuests).toFixed(2);
+
 kioskCheque += ' ₽';
 let trios = triosN + ' 🥖🥤🍪';
 output = `${datey}:
 
 Выручка общая: ${kioskSum+cashierBoxSum} ₽
 Киоск, продажи: ${kioskSum} ₽
-Киоск, чек: ${kioskCheque}
+Киоск, чек: ${fixDecimals(kioskCheque)}
 Касса, продажи: ${cashierBoxSum} ₽
-Касса, чек: ${(cashierBoxSum/cashierGuests).toFixed(2)} ₽
+Касса, чек: ${fixDecimals((cashierBoxSum/cashierGuests).toFixed(2))} ₽
 Количество гостей: (${kioskGuests}+${cashierGuests})=${kioskGuests+cashierGuests} 👥
-Средний чек: ${((kioskSum+cashierBoxSum)/(kioskGuests+cashierGuests)).toFixed(2)} ₽
+Средний чек: ${fixDecimals(((kioskSum+cashierBoxSum)/(kioskGuests+cashierGuests)).toFixed(2))} ₽
 Напитки: ${cashierDrinks}
 Офф​лайн: ${(kioskGuests+cashierGuests)-yandexDeliveries} 👤
 `
@@ -107,3 +145,5 @@ kioskPreSum.addEventListener('keyup',()=>{
   }
   document.querySelector('#kioskSum').value=vals.reduce((acc, num) => acc + Number(num), 0);
 },false);
+
+document.addEventListener("DOMContentLoaded",recallSettings());
